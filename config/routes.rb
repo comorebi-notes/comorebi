b40cf2676bda7e2bedb3857e8f97b5a139a25ed9
kero-uzura <biruge@biruge.com>
2017-01-14 01:39:06 +0900

    add rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top/index'
+
+  root to: 'top#index'
+
+  namespace :admin do
+    root to: 'users#index'
+    resources :users
+  end
 end

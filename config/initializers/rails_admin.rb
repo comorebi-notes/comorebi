7dc2f670b461dba1035191b42f0123188ac117c2
kero-uzura <biruge@biruge.com>
2017-01-15 18:54:34 +0900

    refactor header

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,7 @@ RailsAdmin.config do |config|
   config.authenticate_with do
     warden.authenticate! scope: :admin
   end
-  config.current_user_method(&:current_user)
+  config.current_user_method(&:current_admin)
 
   ## == Cancan ==
   # config.authorize_with :cancan
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin/rails_admin', as: 'rails_admin'
-  root to: 'top#index'
-
+  mount RailsAdmin::Engine => '/rails_admin', as: 'rails_admin'
   devise_for :admins, path: :admin, path_names: { sign_in: 'login', sign_out: 'logout' }
+
+  root to: 'top#index'
 
   resource :admin, only: [:show]
   namespace :admin do

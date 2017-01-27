082283577ec1f348a63f550af128a0acb32decea
kero-uzura <biruge@biruge.com>
2017-01-28 01:52:27 +0900

    can destroy and can add status

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,4 +1,4 @@
 class Music < ApplicationRecord
-  has_many :work_musics
+  has_many :work_musics, dependent: :destroy
   has_many :works, through: :work_musics
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
-  has_many :work_musics
+  has_many :work_musics, dependent: :destroy
   has_many :musics, through: :work_musics
   acts_as_taggable_on :tags
 

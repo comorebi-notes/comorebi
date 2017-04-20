4fbb7d1ad853985811c34d0672cbe859d8354e06
kero-uzura <wakabayashita@firstserver.co.jp>
2017-04-20 11:32:12 +0900

    refactoring

--- client/app/bundles/admin/api/getParams.js
+++ client/app/bundles/admin/api/getParams.js
@@ -1,33 +1,27 @@
-export const getParams = (data, type) => {
-  switch (type) {
-  case "editAdmin": {
-    const { name, email, password, password_confirmation, current_password } = data
-    return {
-      admin: {
-        name: name || '',
-        email: email || '',
-        password: password || '',
-        password_confirmation: password_confirmation || '',
-        current_password: current_password || ''
-      }
+export const editAdmin = (data) => {
+  const { name, email, password, password_confirmation, current_password } = data
+  return {
+    admin: {
+      name: name || '',
+      email: email || '',
+      password: password || '',
+      password_confirmation: password_confirmation || '',
+      current_password: current_password || ''
     }
   }
-  case "editWork": {
-    const { title, description, status, categories, tags, published_date, published_time, item_ids } = data
-    return {
-      work: {
-        title,
-        description,
-        status,
-        category_list: categories,
-        tag_list:      tags,
-        published_at:  `${published_date}T${published_time}`,
-        music_ids:     item_ids.musics
-      }
-    }
-  }
-  }
-  return {}
 }
 
-export default getParams
+export const editWork = (data) => {
+  const { title, description, status, categories, tags, published_date, published_time, item_ids } = data
+  return {
+    work: {
+      title,
+      description,
+      status,
+      category_list: categories,
+      tag_list:      tags,
+      published_at:  `${published_date}T${published_time}`,
+      music_ids:     item_ids.musics
+    }
+  }
+}

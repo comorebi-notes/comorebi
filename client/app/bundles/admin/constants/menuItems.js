6a5f0133aaa6b0c21c2299cc18d965182cf0d429
kero-uzura <biruge@biruge.com>
2017-04-26 23:11:13 +0900

    modify categories

--- client/app/bundles/admin/constants/menuItems.js
+++ client/app/bundles/admin/constants/menuItems.js
@@ -2,7 +2,7 @@ const menuItems = [
   { label: "Works",    url: "",         icon: "star" },
   { label: "Musics",   url: "musics",   icon: "music" },
   { label: "Images",   url: "images",   icon: "picture-o" },
-  { label: "Movies",   url: "movies",   icon: "video-camera" },
+  { label: "Designs",  url: "designs",  icon: "address-card" },
   { label: "WebSites", url: "websites", icon: "globe" },
   { label: "Others",   url: "others",   icon: "file" }
 ]

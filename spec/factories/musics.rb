777f3a950f99a5cacdd44055786a0fdd1f50ad5c
kero-uzura <biruge@biruge.com>
2017-05-08 23:47:55 +0900

    fix rspec

--- spec/factories/musics.rb
+++ spec/factories/musics.rb
@@ -1,7 +1,9 @@
+include ActionDispatch::TestProcess
+
 FactoryGirl.define do
   factory :music do
     title      { generate :title }
     lyrics     { generate :description }
-    sound_file "..."
+    sound_file { fixture_file_upload("spec/fixtures/sound_file/sample.mp3", "audio/mp3") }
   end
 end
--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe Music, type: :model do
   let(:articles) { create_list(:article, 3) }
   let(:music) { create(:music, article_ids: articles.map { |article| article.id }) }
+  let(:temp_sound) { create(:temp_sound) }
 
-  it "is valid with title" do
-    music = Music.new(title: "hoge", sound_file: "fuga")
+  it "is valid with title and sound file" do
+    music = Music.new(title: "hoge", sound_file: temp_sound.file)
     expect(music).to be_valid
   end
 
-  it "is invalid without title" do
+  it "is invalid without title and sound file" do
     music = Music.new()
     expect(music).not_to be_valid
   end
